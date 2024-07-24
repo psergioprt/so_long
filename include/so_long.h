243904fc39ea17ccfd638927b2dc050be899991a
psergioprt <paulo.santossilva@gmail.com>
2024-07-24 14:16:38 +0100

    update so_long

--- include/so_long.h
+++ include/so_long.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 11:47:23 by pauldos-          #+#    #+#             */
+/*   Updated: 2024/07/24 11:47:56 by pauldos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
@@ -9,8 +21,15 @@
 # include "../util_funcs/Libft/libft.h"
 # include "../util_funcs/get_next_line/get_next_line.h"
 
+typedef struct s_game
+{
+	int		line_count;
+	int		max_line_length;
+	char	**map;
+}	t_game;
+
 void	add_print_lines(char ***map, int fd);
 void	mem_free(char ***map, int line_count);
-int	map_mem_allocate(char ***map, int fd, int line_ct, int max_line_lth); 
+int		map_mem_allocate(char ***map, int fd, int line_ct, int max_line_lth);
 
 #endif
--- src/so_long.c
+++ src/so_long.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "so_long.h"
-#define MAP_FILE "map.ber"
 
 static void	map_read(int fd, int *line_count, int *max_line_length)
 {
@@ -30,25 +29,30 @@ static void	map_read(int fd, int *line_count, int *max_line_length)
 	}
 }
 
+void	init_game(t_game *game)
+{
+	game->line_count = 0;
+	game->max_line_length = 0;
+	game->map = NULL;
+}
+
 int	main(int argc, char *argv[])
 {
 	int		fd;
-	int		line_count;
-	int		max_line_length;
-	char	**map;
+	t_game	game;	
 
 	if (argc != 2)
 		return (1);
-	line_count = 0;
-	max_line_length = 0;
+	init_game(&game);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (1);
-	map_read(fd, &line_count, &max_line_length);
+	map_read(fd, &game.line_count, &game.max_line_length);
 	lseek(fd, 0, SEEK_SET);
-	if (map_mem_allocate(&map, fd, line_count, max_line_length) != 0)
+	if (map_mem_allocate(&game.map, fd, game.line_count, \
+				game.max_line_length) != 0)
 		return (1);
-	add_print_lines(&map, fd);
-	mem_free(&map, line_count);
+	add_print_lines(&game.map, fd);
+	mem_free(&game.map, game.line_count);
 	return (0);
 }
